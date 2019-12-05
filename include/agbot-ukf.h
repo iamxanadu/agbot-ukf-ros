37c5e4ae474523774777618089a073251ef2d9ac
Josef Biberstein <jxb@mit.edu>
2019-12-05 16:16:48 -0500

    Working on tuning covariance

--- include/agbot-ukf.h
+++ include/agbot-ukf.h
@@ -4,6 +4,8 @@
 
 #include "agbot_simulinkblock_20191202.h"
 
+double ax, ay, az, wx, wy, wz;
+
 /*
 qPitch = VAR(1);
 qRoll = VAR(2);
@@ -20,8 +22,7 @@ xDt = VAR(12);
 yDt = VAR(13);
 zDt = VAR(14);
 */
-template <typename Scalar>
-using StateT = typename Eigen::Matrix<Scalar, 14, 1>;
+template <typename Scalar> using StateT = typename Eigen::Matrix<Scalar, 14, 1>;
 
 template <typename Scalar>
 using CovarT = typename Eigen::Matrix<Scalar, 14, 14>;
@@ -29,72 +30,80 @@ using CovarT = typename Eigen::Matrix<Scalar, 14, 14>;
 template <typename Scalar>
 using ControlT = typename Eigen::Matrix<Scalar, 2, 1>;
 
-template <typename Scalar>
-using ObsT = typename Eigen::Matrix<Scalar, 6, 1>;
+template <typename Scalar> using ObsT = typename Eigen::Matrix<Scalar, 6, 1>;
 
 template <typename Scalar>
 using ObsCovarT = typename Eigen::Matrix<Scalar, 6, 6>;
 
 /* process model: x_{k+1} = f(x_k, u_k) + q */
-template <typename Scalar>
-struct process_t
-{
+template <typename Scalar> struct process_t {
 
-    using scalar_t = Scalar;
-    using state_t = StateT<scalar_t>;
-    using control_t = ControlT<scalar_t>;
+  using scalar_t = Scalar;
+  using state_t = StateT<scalar_t>;
+  using control_t = ControlT<scalar_t>;
 
-    inline bool operator()(Eigen::Ref<state_t> state_k,
-                           const Eigen::Ref<const control_t> &control_k,
-                           const Eigen::Ref<const state_t> &proc_noise_k,
-                           const Scalar del_k) const
-    {
+  inline bool operator()(Eigen::Ref<state_t> state_k,
+                         const Eigen::Ref<const control_t> &control_k,
+                         const Eigen::Ref<const state_t> &proc_noise_k,
+                         const Scalar del_k) const {
 
-        // Calculate the instantaneous state derivative using the cont. model
-        double q_lmdt = control_k(0, 0);
-        double q_rmdt = control_k(1, 0);
-        state_t del_state;
-        double * VARp = del_state.data();
-        const double * VAR = state_k.data();
-        double ax, ay, az, wx, wy, wz;
+    // Calculate the instantaneous state derivative using the cont. model
+    double q_lmdt = control_k(0, 0);
+    double q_rmdt = control_k(1, 0);
+    state_t del_state;
+    double *VARp = del_state.data();
+    const double *VAR = state_k.data();
+    
 
-        agbot_simulinkblock_20191202(q_lmdt, q_rmdt, VAR, VARp, &ax, &ay, &az, &wx, &wy, &wz);
+    // ROS_INFO_STREAM_THROTTLE(0.01, "left enc " << std::endl << q_lmdt);
+    // ROS_INFO_STREAM_THROTTLE(0.01, "right enc " << std::endl << q_rmdt);
+    // ROS_INFO_STREAM_THROTTLE(0.01, "state " << std::endl << state_k);
 
-        // Use state derivative to calculate linear approximation of state transition over interval del_k
-        state_k += del_k * del_state;
-        // Optional additive process noise. Probably set this to zero?
-        state_k += proc_noise_k; /* x_{k+1} = x_{k+1} + q (additive process noise)*/
+    agbot_simulinkblock_20191202(q_lmdt, q_rmdt, VAR, VARp, &ax, &ay, &az, &wx,
+                                 &wy, &wz);
 
-        return true;
+    // Prune off small transitions as zero
+    if (q_lmdt == 0.0 && q_rmdt == 0.0) {
+      for (int i = 0; i < 14; i++) {
+        if (del_state[i] < 0.05) {
+          del_state[i] = 0.0;
+        }
+      }
     }
+
+    // ROS_INFO_STREAM_THROTTLE(0.01, "State delta " << std::endl << del_state);
+    // ROS_INFO_STREAM_THROTTLE(0.01, "proc noise " << std::endl << proc_noise_k);
+
+    // Use state derivative to calculate linear approximation of state
+    // transition over interval del_k
+    state_k += del_k * del_state;
+    // Optional additive process noise. Probably set this to zero?
+    state_k += proc_noise_k; /* x_{k+1} = x_{k+1} + q (additive process noise)*/
+
+    return true;
+  }
 };
 
 /* observation model: z_k = h(x_k) + r_k */
-template <typename Scalar>
-struct observe_t
-{
+template <typename Scalar> struct observe_t {
 
-    using scalar_t = Scalar;
-    using state_t = StateT<scalar_t>;
-    using obs_t = ObsT<scalar_t>;
+  using scalar_t = Scalar;
+  using state_t = StateT<scalar_t>;
+  using obs_t = ObsT<scalar_t>;
 
-    inline bool operator()(const Eigen::Ref<const state_t> &state_k,
-                           Eigen::Ref<obs_t> obs_k,
-                           const Eigen::Ref<const obs_t> &obs_noise_k) const
-    {
-        double VARp[14];
-        double ax, ay, az, wx, wy, wz;
-        agbot_simulinkblock_20191202(0, 0, state_k.data(), VARp, &ax, &ay, &az, &wx, &wy, &wz);
+  inline bool operator()(const Eigen::Ref<const state_t> &state_k,
+                         Eigen::Ref<obs_t> obs_k,
+                         const Eigen::Ref<const obs_t> &obs_noise_k) const {
 
-        obs_k[0] = ax;
-        obs_k[1] = ay;
-        obs_k[2] = az;
-        obs_k[3] = wx;
-        obs_k[4] = wy;
-        obs_k[5] = wz;
+    obs_k[0] = ax;
+    obs_k[1] = ay;
+    obs_k[2] = az;
+    obs_k[3] = wx;
+    obs_k[4] = wy;
+    obs_k[5] = wz;
 
-        obs_k += obs_noise_k; /* z_k = z_k + r (additive observation noise) */
+    obs_k += obs_noise_k; /* z_k = z_k + r (additive observation noise) */
 
-        return true;
-    }
+    return true;
+  }
 };
--- src/agbot_ukf_node.cpp
+++ src/agbot_ukf_node.cpp
@@ -1,11 +1,8 @@
-#include <ros/ros.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
-#include <sensor_msgs/Imu.h>
-#include <agbot_ukf/StampedInt.h>
-#include <ukf.h>
 #include "agbot_simulinkblock_20191202_initialize.h"
+#include <agbot_ukf/StampedInt.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <ukf.h>
 
 #include "agbot-ukf.h"
 
@@ -14,6 +11,9 @@ std::string top_imu_raw;
 std::string top_imu_filtered;
 std::string top_l_motor_rate, top_r_motor_rate;
 
+// Loop rate to try to enforce in Hz
+int loop_rate;
+
 ros::Publisher imu_filtered;
 
 // Current encoder measurements - system input
@@ -21,117 +21,114 @@ double ul, ur; // rad/s
 
 spkf::UKF<process_t<double>, observe_t<double>> *ukf;
 
-void imuRawCallback(const sensor_msgs::Imu::ConstPtr &msg)
-{
-    ObsT<double> observ;
-    observ[0] = msg->linear_acceleration.x;
-    observ[1] = msg->linear_acceleration.y;
-    observ[2] = msg->linear_acceleration.z;
-    observ[3] = msg->angular_velocity.x;
-    observ[4] = msg->angular_velocity.y;
-    observ[5] = msg->angular_velocity.z;
-    ukf->innovate(observ, ObsT<double>::Zero());
-    ukf->update();
+void imuRawCallback(const sensor_msgs::Imu::ConstPtr &msg) {
+  ROS_INFO_STREAM_THROTTLE(0.1, "IMU msg " << std::endl << msg->linear_acceleration);
 
-    // Republished filtered message
-    sensor_msgs::Imu nimu;
-    nimu.header = msg->header;
+  ObsT<double> observ;
+  observ[0] = msg->linear_acceleration.x;
+  observ[1] = msg->linear_acceleration.y;
+  observ[2] = msg->linear_acceleration.z;
+  observ[3] = msg->angular_velocity.x;
+  observ[4] = msg->angular_velocity.y;
+  observ[5] = msg->angular_velocity.z;
+  ukf->innovate(observ, ObsT<double>::Zero());
+  ukf->update();
 
-    double VARp[14];
-    double ax, ay, az, wx, wy, wz;
-    agbot_simulinkblock_20191202(ul, ur, ukf->state().data(), VARp, &ax, &ay, &az, &wx, &wy, &wz);
+  // Republished filtered message
+  sensor_msgs::Imu nimu;
+  nimu.header = msg->header;
 
-    nimu.angular_velocity.x = wx; // TODO(josef) these may be in the wrong order because roll pitch yaw do not specify the imu axes. Might be fixed b/c using model funct.
-    nimu.angular_velocity.y = wy;
-    nimu.angular_velocity.z = wz;
-    nimu.linear_acceleration.x = ax;
-    nimu.linear_acceleration.y = ay;
-    nimu.linear_acceleration.z = az;
+  double VARp[14];
+  double fax, fay, faz, fwx, fwy, fwz;
+  agbot_simulinkblock_20191202(ul, ur, ukf->state().data(), VARp, &fax, &fay,
+                               &faz, &fwx, &fwy, &fwz);
 
-    imu_filtered.publish(nimu); // Publish filtered IMU
+  nimu.angular_velocity.x =
+      fwx; // TODO(josef) these may be in the wrong order because roll pitch yaw
+          // do not specify the imu axes. Might be fixed b/c using model funct.
+  nimu.angular_velocity.y = fwy;
+  nimu.angular_velocity.z = fwz;
+  nimu.linear_acceleration.x = fax;
+  nimu.linear_acceleration.y = fay;
+  nimu.linear_acceleration.z = faz;
+
+  imu_filtered.publish(nimu); // Publish filtered IMU
 }
 
-void encoderCallback(const agbot_ukf::StampedInt::ConstPtr &sp1, const agbot_ukf::StampedInt::ConstPtr &sp2)
-{
-    // TODO (josef) these may be in degrees/s; double check with Marc to find out which the func takes
-    ul = sp1->data * -2 * M_PI / (1.079 * 2400);
-    ur = sp1->data * 2 * M_PI / (1.070 * 2400);
+// TODO (josef) might want to sync these up later. Didn't seem to work first
+// time I tried.
+void leftSpeedCallback(const agbot_ukf::StampedInt::ConstPtr &msg) {
+  ul = msg->data * -2 * M_PI / (1.079 * 2400);
+  ROS_INFO_STREAM_THROTTLE(0.1, "Left encoder " << ul);
 }
 
-int main(int argc, char **argv)
-{
+void rightSpeedCallback(const agbot_ukf::StampedInt::ConstPtr &msg) {
+  ur = msg->data * 2 * M_PI / (1.070 * 2400);
+  ROS_INFO_STREAM_THROTTLE(0.1, "Right encoder " << ur);
+}
 
-    ros::init(argc, argv, "ukf");
+int main(int argc, char **argv) {
 
-    agbot_simulinkblock_20191202_initialize();
+  ros::init(argc, argv, "ukf");
 
-    // ROS NodeHandle
-    ros::NodeHandle nh;
+  agbot_simulinkblock_20191202_initialize();
 
-    // Get parameters for topic names
-    nh.param<std::string>("IMU_RAW_TOPIC", top_imu_raw, "/camera/imu");
-    nh.param<std::string>("IMU_FILTERED_TOPIC", top_imu_filtered, "/ukf/imu");
-    nh.param<std::string>("L_MOTOR_RATE_TOPIC", top_l_motor_rate, "/encoder1");
-    nh.param<std::string>("R_MOTOR_RATE_TOPIC", top_r_motor_rate, "/encoder2");
+  // ROS NodeHandle
+  ros::NodeHandle nh;
 
-    // ROS subscribers and publishers
-    ros::Subscriber imu_raw = nh.subscribe(top_imu_raw, 1, imuRawCallback);
-    message_filters::Subscriber<agbot_ukf::StampedInt> l_motor_rate(nh, top_l_motor_rate, 1);
-    message_filters::Subscriber<agbot_ukf::StampedInt> r_motor_rate(nh, top_r_motor_rate, 1); // Control inputs (Int64 is a placeholder for StampedInt)
-    imu_filtered = nh.advertise<sensor_msgs::Imu>(top_imu_filtered, 100);
-    
-    // Approx time sync pol for encoders
-    typedef message_filters::sync_policies::ApproximateTime<agbot_ukf::StampedInt, agbot_ukf::StampedInt> EncoderSyncPolicy;
+  // Get parameters for topic names
+  nh.param<std::string>("IMU_RAW_TOPIC", top_imu_raw, "/camera/imu");
+  nh.param<std::string>("IMU_FILTERED_TOPIC", top_imu_filtered, "/ukf/imu");
+  nh.param<std::string>("L_MOTOR_RATE_TOPIC", top_l_motor_rate, "/speed1");
+  nh.param<std::string>("R_MOTOR_RATE_TOPIC", top_r_motor_rate, "/speed2");
 
-    // Syncronizer
-    message_filters::Synchronizer<EncoderSyncPolicy> sync(EncoderSyncPolicy(10), l_motor_rate, r_motor_rate);
-    sync.registerCallback(boost::bind(&encoderCallback, _1, _2));
+  nh.param<int>("LOOP_RATE", loop_rate, 1000);
 
-    // Create intial values for the UKF
-    StateT<double> state;
-    // y p r is zero
-    state[0] = 0;
-    state[1] = 0;
-    state[2] = 0;
-    // qlm qrm zero
-    state[3] = 0;
-    state[4] = 0;
-    // x y z zero
-    state[5] = 0;
-    state[6] = 0;
-    state[7] = 0;
-    // y p r rates zero
-    state[8] = 0;
-    state[9] = 0;
-    state[10] = 0;
-    // velocities zero
-    state[11] = 0;
-    state[12] = 0;
-    state[13] = 0;
+  // ROS subscribers and publishers
+  ros::Subscriber imu_raw = nh.subscribe(top_imu_raw, 1, imuRawCallback);
+  ros::Subscriber l_motor_rate =
+      nh.subscribe(top_l_motor_rate, 1, leftSpeedCallback);
+  ros::Subscriber r_motor_rate =
+      nh.subscribe(top_r_motor_rate, 1, rightSpeedCallback);
+  imu_filtered = nh.advertise<sensor_msgs::Imu>(top_imu_filtered, 100);
 
-    const auto covar = 10.0 * CovarT<double>::Identity();
-    const auto procovar = 5.0 * CovarT<double>::Identity();
-    const auto obscovar = 3.0 * ObsCovarT<double>::Identity();
+  // Create intial values for the UKF
+  StateT<double> state = StateT<double>::Zero();
+  // TODO (josef) Magic initial states?
+  state[0] = -1.4826 * 0.0174532925199433;
+  state[7] = 0.165;
 
-    ControlT<double> control;
+  const auto covar = 0.00001 * CovarT<double>::Identity();
+  const auto procovar = 0.00001 * CovarT<double>::Identity();
+  const auto obscovar = 0.00005 * ObsCovarT<double>::Identity();
 
-    StateT<double> proc_noise = StateT<double>::Zero();
-    ObsT<double> obs_noise = ObsT<double>::Zero();
+  ControlT<double> control;
 
-    // Create the UKF
-    ukf = new spkf::UKF<process_t<double>, observe_t<double>>(state, covar, procovar, obscovar);
+  StateT<double> proc_noise = StateT<double>::Zero();
+  ObsT<double> obs_noise = ObsT<double>::Zero();
 
-    ros::Rate r(100); // 1000 Hz max loop rate
-    while (ros::ok())
-    {
-        control[0] = ul;
-        control[1] = ur;
-        ukf->predict(control, 0.001, proc_noise);
+  // Create the UKF
+  ukf = new spkf::UKF<process_t<double>, observe_t<double>>(state, covar,
+                                                            procovar, obscovar);
 
-        ros::spinOnce();
+  ros::Rate r(loop_rate); // 1000 Hz default loop rate
+  while (ros::ok()) {
 
-        ROS_INFO_STREAM("Was I able to meet the loop rate? " << std::boolalpha << r.sleep());
-    }
+    control[0] = ul;
+    control[1] = ur;
+    ukf->predict(control, 1 / float(loop_rate),
+                 proc_noise); // Run a dead reconing step
+    ros::spinOnce();          // Will innovate here if get an accel measurement
 
-    return 0;
+    ROS_INFO_STREAM_THROTTLE(0.01, "Current Kalman state " << std::endl
+                                                           << ukf->state());
+
+    bool sleep_success = r.sleep();
+
+    ROS_INFO_STREAM_THROTTLE(0.1, "Was I able to meet the loop rate of "
+                                      << loop_rate << " Hz? " << std::boolalpha
+                                      << sleep_success);
+  }
+
+  return 0;
 }
